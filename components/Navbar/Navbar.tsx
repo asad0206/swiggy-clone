6f3962160d990439b9801900dfb0e1d8df3189e6
Asad <asad200ana@gmail.com>
2024-11-15 12:03:27 +0530

    Navbar made sticky and on top

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-import Search from "@/components/search";
+import Search from "@/components/ui/search";
 import Link from "next/link";
 import Image from "next/image";
 export default function Navbar() {
     return (
-        <div className="max-w-screen flex flex-row mx-10 my-8 p-5 bg-white shadow-lg drop-shadow-lg rounded-md hover:shadow-orange-200">
+        <nav className="max-w-screen flex flex-row mx-10 my-8 p-5 bg-white shadow-lg drop-shadow-lg rounded-md hover:shadow-orange-200 sticky top-0 z-10">
             <div className="w-1/2 h-full justify-start rounded-lg">
                 <Link href={"/"}>
                     <Image src="/swiggy-logo.webp" alt="Swiggy Logo" width={150} height={100} />
@@ -12,7 +12,7 @@ export default function Navbar() {
             <div className="w-1/2 h-full flex justify-end">
                 <Search />
             </div>
-        </div>
+        </nav>
     );
 
 }
